c150c1b4a3b0e04346344b0e80d74c49e1218602
Gabriel <g.alves.m.2008@gmail.com>
2020-06-27 14:40:45 -0300

    fix mapper

--- lib/data/mapper/remote_to_domain.dart
+++ lib/data/mapper/remote_to_domain.dart
@@ -3,11 +3,8 @@ import 'package:domain/model/house.dart';
 import '../remote/model/house_rm.dart';
 
 extension HouseRMMapperToDM on HouseRM {
-  House toDM() {
-    print('remote to dm $image');
-    return House(
-      name: name,
-      image: image,
-    );
-  }
+  House toDM() => House(
+        name: name,
+        image: image,
+      );
 }
