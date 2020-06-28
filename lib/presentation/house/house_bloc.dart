40917ce3386ef393a3f6082d39b79f73747f9b0c
Gabriel <g.alves.m.2008@gmail.com>
2020-06-28 03:36:35 -0300

    Implement bloc lib

--- lib/presentation/house/house_bloc.dart
+++ lib/presentation/house/house_bloc.dart
@@ -1,40 +1,28 @@
+import 'package:bloc/bloc.dart';
 import 'package:domain/use_case/get_house_list_uc.dart';
 import 'package:flutter/foundation.dart';
-import 'package:rxdart/rxdart.dart';
 
-import '../../common/subscription_holder.dart';
 import 'house_mapper.dart';
 import 'house_models.dart';
 
-class HouseBloc with SubscriptionHolder {
+class HouseBloc extends Bloc<HouseEvent, HouseState> {
   HouseBloc({
     @required this.getHouseListUC,
   }) : assert(getHouseListUC != null) {
-    Rx.merge(
-      List<Stream<void>>.of([
-        Stream.value(null),
-        _onTryAgainSubject,
-      ]),
-    )
-        .flatMap(
-          (_) => _getHouseState(),
-        )
-        .listen(
-          _onNewStateSubject.add,
-        );
+    Stream.value(null).listen(
+      (_) => add(
+        OnInitEvent(),
+      ),
+    );
   }
 
   final GetHouseListUC getHouseListUC;
 
-  final _onNewStateSubject = BehaviorSubject<HouseState>.seeded(Loading());
+  @override
+  HouseState get initialState => Loading();
 
-  Stream<HouseState> get onNewState => _onNewStateSubject.stream;
-
-  final _onTryAgainSubject = PublishSubject<void>();
-
-  Sink<void> get onTryAgainSink => _onTryAgainSubject.sink;
-
-  Stream<HouseState> _getHouseState() async* {
+  @override
+  Stream<HouseState> mapEventToState(HouseEvent event) async* {
     yield Loading();
 
     try {
@@ -50,11 +38,5 @@ class HouseBloc with SubscriptionHolder {
     } catch (e) {
       yield Error();
     }
-  }
-
-  dispose() {
-    _onNewStateSubject.close();
-    _onTryAgainSubject.close();
-    super.disposeAll();
   }
 }
